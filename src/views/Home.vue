e7d8d32cf6131d3d248b61ca5efb31349b71386c
wangjingfan <18713155828@163.com>
2020-05-09 17:49:08 +0800

    测试提交代码-王靖凡-2020年5月9日17:49:04

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello 靖凡大人！"/>
   </div>
 </template>
 
