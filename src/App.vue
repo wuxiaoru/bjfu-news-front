8254c84c7951917040354a8bdd3702033a13a9e2
wangjingfan <18713155828@163.com>
2020-05-10 09:02:59 +0800

    测试上传-王靖凡-2020-5-10 09:02:57

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">首页</router-link> |
+      <router-link to="/">首页首页首页</router-link> |
       <router-link to="/about">关于</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
