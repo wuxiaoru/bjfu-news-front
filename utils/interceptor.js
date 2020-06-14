4470ef6036a94937a60abd85f60aa5f7bcdafa56
14_haopingping <18832655903@163.com>
2020-06-14 20:52:49 +0800

    新闻列表的接口调试-郝平平-2020-6-14 20:52:42

--- utils/interceptor.js
+++ utils/interceptor.js
@@ -10,42 +10,42 @@ axios.defaults.baseURL = process.env.baseURL
 
 // 全局添加拦截器的作用是可以在每个API前面加上headers的token验证
 axios.interceptors.request.use(config => {
-    // 判断token是否存在和是否需要token验证的路由
-    if (store.state.token) {
-        config.headers.xToken = 'token';
-    };
-    return config;
+  // 判断token是否存在和是否需要token验证的路由
+  if (store.state.token) {
+    // config.headers.xToken = 'token';
+  };
+  return config;
 })
 
 // 处理退出响应的拦截器 err可以捕获状态，来进行响应的处理
 axios.interceptors.response.use(response => {
-    if (response.status === 200) {
-        const res = response.data;
-        // 如果code是-1 说明用户注销或者token已经过期了 需要消除localstorage和vuex中的token
-        if (res.code === -1) {
-            clearHandler();
-        }
+  if (response.status === 200) {
+    const res = response.data;
+    // 如果code是-1 说明用户注销或者token已经过期了 需要消除localstorage和vuex中的token
+    if (res.code === -1) {
+      clearHandler();
     }
-    return response;
+  }
+  return response;
 },
-err=> {
+  err => {
     // 判断是否授权
     if (err.response.status === 401) {
-        clearHandler();
+      clearHandler();
     }
-});
+  });
 
 // 消除localstorage和vuex中的token
 function clearHandler() {
-    localStorage.removeItem('token');
-    store.commit('setToken', '');
-    // 跳转首页
-    router.push({
-        path: '/home',
-        query: {
-            redirect: router.currentRoute.path
-        }
-    });
+  localStorage.removeItem('token');
+  store.commit('setToken', '');
+  // 跳转首页
+  router.push({
+    path: '/home',
+    query: {
+      redirect: router.currentRoute.path
+    }
+  });
 }
 
 export default axios
